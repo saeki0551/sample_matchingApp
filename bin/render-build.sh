2bb65f9d4d4cc24119e0fb1dfa8fe2db8fd79fdc
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-04 21:58:25 +0900

    本番環境のDBの設定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
+
+# Exit on error
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bin/rails assets:precompile
+bin/rails assets:clean
+
+# If you have a paid instance type, we recommend moving
+# database migrations like this one from the build command
+# to the pre-deploy command:
+bin/rails db:migrate
