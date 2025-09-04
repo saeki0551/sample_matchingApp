678a0a9c7ee189b5c199286e5770429909e98438
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-04 21:55:30 +0900

    本番環境のDBの設定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
+# exit on error
 set -o errexit
 
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
-
-bin/rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
