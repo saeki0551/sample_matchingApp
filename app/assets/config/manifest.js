7952830c1f27dcb8b16334c2393662c255a25c7c
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-06 16:13:11 +0900

    /opt/render/project/src/app/assets/config/manifest.js:5

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //= link_tree ../stylesheets .css
 //= link application.js
 //= link controllers/application.js
-//= link controllers/hello_cotroller.js
+//= link controllers/hello_controller.js
 //= link controllers/index.js
 
