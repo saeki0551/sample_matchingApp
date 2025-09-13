5b2274f451d21cd0553048a591fcd22c6fc1e1df
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-13 15:35:18 +0900

    サインップ後のパス変更

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,7 +1,7 @@
-class Prefecture < ActiveHash::Base
+class Hobby < ActiveHash::Base
   self.data = [
       {id: 1, name: 'ゲーム'}, {id: 2, name: 'アニメ・ドラマ'}, {id: 3, name: 'スポーツ'},
-      {id: 4, name: '読書'}, {id: 5, name: '運動'}, {id: 6, name: '旅行'},
+      {id: 4, name: '読書'}, {id: 5, name: '運動'}, {id: 6, name: '旅行'}
   ]
 
   include ActiveHash::Associations
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "tops#index"
+  resources :users
 end
