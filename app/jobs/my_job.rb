6b5d72c693b0984ca3816feb2dceffb9cb5eeaa9
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-17 19:53:13 +0900

    amazons3デプロイエラー

--- app/jobs/my_job.rb
+++ app/jobs/my_job.rb
@@ -1,7 +1,8 @@
 class MyJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(arg1:)
+    # ここにジョブで実行したい処理を書く
+    puts "Job executed with arg1=#{arg1}"
   end
 end
