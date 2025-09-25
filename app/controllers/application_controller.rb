97d53f77bbb60c6e9ec96b6444671461fbbdf1d7
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-25 23:23:45 +0900

    カラム編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+<<<<<<< HEAD
   before_action :authenticate_user!
+=======
+>>>>>>> 6be23a0 (カラム編集)
   before_action :configure_permitted_parameters , if: :devise_controller?
 
   protected
