e45e0b1dfebbfb961fd79b12d00b3fbd3424842b
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-26 00:13:47 +0900

    カラム

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,4 @@ Rails.application.routes.draw do
 
   get "user_informations/new", to: "user_informations#new", as: :new_user_information
   post "user_informations/create", to: "user_informations#create", as: :create_user_information
-  
 end
