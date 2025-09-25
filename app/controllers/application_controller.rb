ac807b1a7327adf866661e1f1b7ef9ab83bbf754
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-09-25 23:23:45 +0900

    カラム編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 =======
 >>>>>>> 6be23a0 (カラム編集)
   before_action :configure_permitted_parameters , if: :devise_controller?
-
+<<<<<<<
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
