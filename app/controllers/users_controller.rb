73c93fa9191252bb78524b1df6dc1b8db48a367b
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-10-01 17:30:50 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class UsersController < ApplicationController
 
   def index
@@ -20,3 +21,40 @@ class UsersController < ApplicationController
     end
   end
 end
+=======
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = current_user
+    if @user.update(user_params)
+      # binding.pry
+      redirect_to users_path
+    else
+      render :back
+    end 
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+      redirect_to user_session_path, notice: "アカウントの削除が完了しました"
+    else
+      flash.now[:alert] = "アカウントの削除に失敗しました"
+      render :index
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :age, :birth_date, :prefecture_id, :hobby_id)
+  end
+end
+>>>>>>> 454104b2d7ce57be6e242632dccf5b4f8cc3da2e
