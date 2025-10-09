61ed4f85bd156e10a7f73897c1db02b192b0a988
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-10-09 14:39:13 +0900

    アカウント作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[new create]
+  skip_before_action :authenticate_user!, only: %i[new]
 
   def index
     @users = User.all
@@ -8,13 +8,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
-  def create
-    @user = User.new(create_users_params)
-    session[:user_form] = @user
-    redirect_to controller: :user_informations, action: :new, notice: "プロフィール情報を下さい。"
-  end
-
   
   def destroy
     user = User.find(params[:id])
