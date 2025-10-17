9362965bbc61364476158c6f5a562b700b57c9dc
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-10-17 21:24:03 +0900

    ストロングパラメータの修正

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -6,5 +6,11 @@ class UserInformationsController < ApplicationController
     @user.build_user_information
     # @user_information = UserInformation.new
   end
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
 end
 
