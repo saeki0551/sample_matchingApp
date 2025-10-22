34ab0077ab67ba9992b1ec198544ba75a3d35294
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-10-22 22:09:27 +0900

    デフォルトユーザーアイコン画像のコールバック実装

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -5,9 +5,6 @@ class UserInformationsController < ApplicationController
 
   def create
     @user_information = UserInformation.new(user_information_params)
-    unless @user_information.image.attached?
-      @user_information.image.attach(io: File.open("app/assets/images/defaultUserIcon.png"), filename: "defaultUserIcon.png", content_type: "image/png")
-    end
     if @user_information.save
       redirect_to users_path
     else
