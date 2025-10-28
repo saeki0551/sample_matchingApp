4b31de13ce0f46540045cc02680d9f420ec7a3a9
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-10-28 19:53:47 +0900

    メソッド名修正

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -6,11 +6,11 @@ class UserInformation < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :hobby
 
-  before_save :default_user_icon_image
+  before_save :default_image_attach
 
   private
 
-    def default_user_icon_image
+    def default_image_attach
       unless self.image.attached?
         self.image.attach(io: File.open("app/assets/images/defaultUserIcon.png"), filename: "defaultUserIcon.png", content_type: "image/png")
       end
