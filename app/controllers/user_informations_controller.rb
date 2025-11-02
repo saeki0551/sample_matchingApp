7dd57368d18b27b16fd652eca879af17ee9d12a1
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-11-02 21:06:33 +0900

    ログイン情報入力送信で失敗処理

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -3,8 +3,14 @@ class UserInformationsController < ApplicationController
 
   def new
     @user = User.new(user_params)
+    return redirect_to  new_user_path, alert: "パスワードが間違っています。" if @user.password != @user.password_confirmation  
+
+    if User.exists?(email: @user.email) 
+      @created_user = User.order(updated_at: :desc).limit(1).find_by(email: @user.email)
+      return redirect_to  new_user_path, alert: "このユーザーは作成済みです。ログインしてください。" if @created_user.present?
+    end  
+    
     @user.build_user_information
-    # @user_information = UserInformation.new
   end
 
   private
