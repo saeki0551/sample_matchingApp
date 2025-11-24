95f11baa03913d6e87c4975bb888eadf1c8bb2af
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-11-24 11:01:03 +0900

    マジックナンバーから定数を利用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters , if: :devise_controller?
 
+  ACCOUNT_STOP_TIME = 100
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
     if user.update(is_deleted: true, cancel_membership_time: DateTime.now)
       sign_out(user)
-      render("tops/cancel_membership")
+      redirect_to controller: :tops, action: :cancel_membership
     else
       redirect_to users_path, alert: "予想外のエラー、退会に失敗しました。"
     end
