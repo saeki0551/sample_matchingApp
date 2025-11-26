3829fcb11ece2f74b7bd19ec86e5dd08a320baa5
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-11-27 07:21:23 +0900

    新規登録画面の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters , if: :devise_controller?
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 
   private 
   def after_sign_in_path_for(resource)
