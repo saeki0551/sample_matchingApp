943106023466a74b0af820a080f6a9035512e032
SaekiNaohiko <nsaekitadayayu@icloud.com>
2025-12-26 07:23:08 +0900

    冗長なコードの修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -65,9 +65,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def create 
+    user = User.new(session[:user])
     if params[:user].present?
       session[:user] = user_params
-      user = User.new(session[:user])
       if User.exists?(email: user.email) 
         return redirect_to  new_user_path, alert: "新規登録できませんでした。再度、新規登録またはログインしてください。"
       end 
@@ -77,7 +77,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
       redirect_to new_user_information_path(user)
     else
       ActiveRecord::Base.transaction do
-        user = User.new(session[:user])
         user.save!
         @user_information.user_id = user.id
         @user_information.save!
