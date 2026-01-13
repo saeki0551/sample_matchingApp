92651e87f1bb7559d0c03400e3ab9c1538558ee6
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-01-13 19:50:35 +0900

    非同期通信

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,9 @@
 class LikesController < ApplicationController
+  protect_from_forgery except: :create #InvalidCrossOriginRequest CSRF(クロスサイトリクエストフォージェリ) CSRF対策として、RailsがフォームやらAjaxやらで、トークンを発行して認証までしてくれています。 ほとんどの場合、何も気にすることなく、勝手に認証されているのではないでしょうか。その、認証に失敗した場合に、InvalidCrossOriginRequestが発生します。
+
   def create
-    like = Like.new(user_id: params[:user_id], liked_user_id: params[:liked_user_id])
-    if like.save
-      redirect_to users_path
-    else
-      redirect_to users_path, alert: "いいねができませんでした。"
-    end
+    @like = Like.new(user_id: params[:user_id], liked_user_id: params[:liked_user_id])
+    @like.save
   end
 
   def destroy
