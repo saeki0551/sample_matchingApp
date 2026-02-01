24c90d7b7c3380a1f4c58cff3b71a48e20a42521
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-02-01 12:18:52 +0900

    退会していたら新規登録できない、10秒後にできる

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -66,11 +66,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create 
     if params[:user].present?
-      session[:user] = user_params
       user = User.new(user_params)
-      if User.exists?(email: user.email) 
+      if User.exists?(email: user.email, is_deleted: false) 
         return redirect_to  new_user_path, alert: "新規登録できませんでした。再度、新規登録またはログインしてください。"
-      end 
+        
+      elsif created_user = User.order(updated_at: :desc).limit(1).select(:is_deleted, :cancel_membership_count, :cancel_membership_time).find_by(email: user.email, is_deleted: true)
+        return redirect_to  new_user_path, alert: "新規登録できませんでした。ログインするか、#{ACCOUNT_STOP_TIME} 秒後に新規登録してください。" if created_user.is_deleted && Time.zone.now - created_user.cancel_membership_time < ACCOUNT_STOP_TIME
+        
+        if created_user.is_deleted && Time.zone.now - created_user.cancel_membership_time >= ACCOUNT_STOP_TIME
+          session[:user][:cancel_membership_count] = created_user.cancel_membership_count + 1
+        end
+      end
+      
       user.check_password
       return redirect_to  new_user_path, alert: user.check_password if user.check_password.present?
       
