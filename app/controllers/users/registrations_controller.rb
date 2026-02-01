2748adcc541009bfba651ddc3f4c051eaf461d3c
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-02-02 07:57:49 +0900

    メソッド化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -66,21 +66,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create 
     if params[:user].present?
-      user = User.new(user_params)
-      if User.exists?(email: user.email, is_deleted: false) 
-        return redirect_to  new_user_path, alert: "新規登録できませんでした。再度、新規登録またはログインしてください。"
-        
-      elsif created_user = User.order(updated_at: :desc).limit(1).select(:is_deleted, :cancel_membership_count, :cancel_membership_time).find_by(email: user.email, is_deleted: true)
-        return redirect_to  new_user_path, alert: "新規登録できませんでした。ログインするか、#{ACCOUNT_STOP_TIME} 秒後に新規登録してください。" if created_user.is_deleted && Time.zone.now - created_user.cancel_membership_time < ACCOUNT_STOP_TIME
-        
-        if created_user.is_deleted && Time.zone.now - created_user.cancel_membership_time >= ACCOUNT_STOP_TIME
-          session[:user][:cancel_membership_count] = created_user.cancel_membership_count + 1
-        end
-      end
-      
+      user = User.new(user_params)        
       user.check_password
       return redirect_to  new_user_path, alert: user.check_password if user.check_password.present?
       
+      user.already_sign_up?
+      return redirect_to  new_user_path, alert: "新規登録できませんでした。再度、新規登録またはログインしてください。" if user.already_sign_up?
+
+      if user.already_cancel_membership?
+        return redirect_to  new_user_path, alert: user.already_cancel_membership? 
+      else
+        created_user = User.order(updated_at: :desc).limit(1).select(:is_deleted, :cancel_membership_count, :cancel_membership_time).find_by(email: user.email, is_deleted: true)
+        user.cancel_membership_count = created_user.cancel_membership_count + 1 
+        session[:user][:cancel_membership_count] = user.cancel_membership_count
+        binding.pry
+      end
+
+
       redirect_to new_user_information_path(user)
     else
       ActiveRecord::Base.transaction do
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,18 @@ class User < ApplicationRecord
     return "パスワード は6文字以上12文字以内である必要があります。" unless self.password.length >= 6 && self.password.length <= 12
     return "パスワード と パスワード確認 が一致していません。" unless self.password == self.password_confirmation 
   end
+
+  def already_sign_up?
+    User.exists?(email: self.email, is_deleted: false) 
+  end
+
+  def already_cancel_membership?
+    if created_user = User.order(updated_at: :desc).limit(1).select(:is_deleted, :cancel_membership_count, :cancel_membership_time).find_by(email: self.email, is_deleted: true)
+      if created_user.is_deleted && Time.zone.now - created_user.cancel_membership_time < ACCOUNT_STOP_TIME
+        return "新規登録できませんでした。ログインするか、#{ACCOUNT_STOP_TIME} 秒後に新規登録してください。" 
+      else
+        return false if created_user.is_deleted && Time.zone.now - created_user.cancel_membership_time >= ACCOUNT_STOP_TIME
+      end
+    end
+  end
 end
