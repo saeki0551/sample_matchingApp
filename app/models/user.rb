55f970468b688035614f2056ead0fd28db80af0f
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-02-04 07:33:26 +0900

    userモデルにlikeモデルがあることの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
     return "パスワード と パスワード確認 が一致していません。" unless self.password == self.password_confirmation 
   end
 
-  def judge_liked(current_user_id, user_id)
-    Like.exists?(user_id: current_user_id, liked_user_id: user_id)
+  def already_liked?(current_user_id, user_id)
+    self.likes.exists?(user_id: current_user_id, liked_user_id: user_id)
   end
 end
