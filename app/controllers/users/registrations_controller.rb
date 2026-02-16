4a8087dde2cbb1e0e2b16d3d66a2d47a0f42cfd1
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-02-17 06:39:51 +0900

    ユーザーの作成をdeviseに任せる？

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,31 +1,25 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  def create 
+    if params[:user].present?
+      session[:user] = user_params
+      redirect_to new_user_information_path
+    else
+      ActiveRecord::Base.transaction do
+        user = User.new(session[:user])
+        user.save!
+        @user_information.user_id = user.id
+        @user_information.save!
+        sign_in(user)
+      end
+      redirect_to users_path, notice: "アカウントの作成に成功しました。"
+    end
+  end
 
-  # def create
-  #   binding.pry
-  # end
+  private
 
-  # def create 
-  #   if params[:user].present?
-  #     session[:user] = user_params
-  #     redirect_to new_user_information_path
-  #   else
-  #     ActiveRecord::Base.transaction do
-  #       user = User.new(session[:user])
-  #       binding.pry
-  #       user.save!
-  #       @user_information.user_id = user.id
-  #       @user_information.save!
-  #       sign_in(user)
-  #     end
-  #     redirect_to users_path, notice: "アカウントの作成に成功しました。"
-  #   end
-  # end
-
-  # private
-
-  #   def user_params
-  #     params.require(:user).permit(:email, :password, :password_confirmation)
-  #   end
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
