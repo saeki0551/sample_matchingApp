fb64f98d988928522460564e8d09d3a56d83957c
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-03-10 11:36:02 +0900

    不必要な改行の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-
   def destroy
     user = User.find(params[:id])
     return redirect_to users_path, alert: "ユーザーidが一致しないため、退会ができません。"  unless user.id == current_user.id
