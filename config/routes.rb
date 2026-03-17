16a05b049324e75cf582770b555de47b6ddd2806
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-03-17 19:14:52 +0900

    ルートコメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -7,27 +7,14 @@ Rails.application.routes.draw do
     get 'sign_up', to: 'users/registrations#new', as: :new_user_registration
     post 'users', to: 'users/registrations#create', as: :create_user_registration
   end
-    
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    
-    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-    # Can be used by load balancers and uptime monitors to verify that the app is live.
-    get "up" => "rails/health#show", as: :rails_health_check
-    
-    # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-    # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-    # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-    
-    # Defines the root path route ("/")
-    root "tops#index"
-    
-    get "users/cancel_membership", to: "users#cancel_membership", as: :cancel_membership_user
-    resources :users
-    
-    get "users/:id/destroy", to: "users#destroy", as: :destroy_user
-    
-    get 'user_informations/new', to: 'user_informations#new', as: :new_user_information
-    post 'user_informations/create', to: 'user_informations#create', as: :create_user_information
-    
-  end
+  root "tops#index"
+  
+  get "users/cancel_membership", to: "users#cancel_membership", as: :cancel_membership_user
+  resources :users
+  
+  get "users/:id/destroy", to: "users#destroy", as: :destroy_user
+  
+  get 'user_informations/new', to: 'user_informations#new', as: :new_user_information
+  post 'user_informations/create', to: 'user_informations#create', as: :create_user_information
+end
   
