09eea407e6848f3b098ce5867daf92054efd9def
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-03-17 09:32:29 +0900

    カウントダウン機能

--- app/javascript/countdown.js
+++ app/javascript/countdown.js
@@ -4,24 +4,52 @@ window.onload = function(){
 
   let targetTime = countDown.textContent;
 	targetTime = parseInt(targetTime);
-	targetTime = new Date().setSeconds(targetTime);
+	targetMinitues = Math.floor(targetTime / 60);
+	targetSeconds = targetTime % 60;
+	
+	targetTime = new Date();
+	targetTime.setMinutes(targetTime.getMinutes() + targetMinitues);
+	targetTime.setSeconds(targetTime.getSeconds() + targetSeconds);
+		
+	const endTime = targetTime.getTime();
 
-	function updateCountDown(){
-		const now = new Date().getTime();
-		const distance = targetTime - now;
-
-		const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-		const seconds = Math.floor(distance % (1000 * 60) / 1000);
-		console.log(seconds);
-
-  	countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
-
-		if(distance < 0){
-			clearInterval(interval);
-			message.textContent = '再度、新規登録できます。';
+  if (localStorage.getItem('endTime')) {
+		function updateCountDown(){
+			const now = new Date().getTime();
+			const endTime = localStorage.getItem('endTime');
+			const distance = endTime - now;
+	
+			const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+			const seconds = Math.floor(distance % (1000 * 60) / 1000);
+	
+			countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
+		
+			if(distance < 0){
+				clearInterval(interval);
+				message.textContent = '再度、新規登録できます。';
+			}
 		}
-  }
+		const interval = setInterval(updateCountDown, 1000);
+		updateCountDown();
+	} else {
+		function updateCountDown(){
+			const now = new Date().getTime();
+			const distance = endTime - now;
+	
+			const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+			const seconds = Math.floor(distance % (1000 * 60) / 1000);
+	
+			countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
+	
+		  localStorage.setItem('endTime', endTime);
 
-	const interval = setInterval(updateCountDown, 1000);
-	updateCountDown();
+			if(distance < 0){
+				clearInterval(interval);
+				message.textContent = '再度、新規登録できます。';
+			}
+		}
+	
+		const interval = setInterval(updateCountDown, 1000);
+		updateCountDown();
+	}
 }
