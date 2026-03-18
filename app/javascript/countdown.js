e839e3b3eaea3ea7ff915f06853757674fa87704
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-03-19 08:47:44 +0900

    配列からメールアドレスを探して取得

--- app/javascript/countdown.js
+++ app/javascript/countdown.js
@@ -16,14 +16,24 @@ window.onload = function(){
 
 	// localStorage.clear();
 
-  if (localStorage.getItem('user_data_array')) {
+	const user_data_array = [];
+	let user_data = {
+		email: email,
+		endTime: endTime
+	}
+
+	user_data_array.push(user_data)
+	// console.log(user_data_array[0])
+
+  // if (user_data_array[0]) {
 		function updateCountDown(){
 			const now = new Date().getTime();
-			const user_data_array = JSON.parse(localStorage.getItem('user_data_array'));
-			console.log(user_data_array)
-			console.log(user_data_array.email)
-			console.log(user_data_array.endTime)
+			// console.log(user_data_array[0]);
 
+			function sort_user(user_data) {
+				return user_data.email === email;
+			}
+			console.log(user_data_array.find(sort_user));
 
 			const distance = user_data_array.email - now;
 	
@@ -39,30 +49,23 @@ window.onload = function(){
 		}
 		const interval = setInterval(updateCountDown, 1000);
 		updateCountDown();
-	} else {
-		function updateCountDown(){
-			const now = new Date().getTime();
-			const distance = endTime - now;
+	// } else {
+	// 	function updateCountDown(){
+	// 		const now = new Date().getTime();
+	// 		const distance = endTime - now;
 	
-			const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-			const seconds = Math.floor(distance % (1000 * 60) / 1000);
+	// 		const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+	// 		const seconds = Math.floor(distance % (1000 * 60) / 1000);
 	
-			countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
-	
-			let user_data = {
-				email: email,
-				endTime: endTime
-			}
-			
-			localStorage.setItem('user_data_array', JSON.stringify(user_data));
+	// 		countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
 
-			if(distance < 0){
-				clearInterval(interval);
-				message.textContent = "再度、新規登録できます。";
-			}
-		}
+	// 		if(distance < 0){
+	// 			clearInterval(interval);
+	// 			message.textContent = "再度、新規登録できます。";
+	// 		}
+	// 	}
 	
-		const interval = setInterval(updateCountDown, 1000);
-		updateCountDown();
-	}
+	// 	const interval = setInterval(updateCountDown, 1000);
+	// 	updateCountDown();
+	// }
 }
