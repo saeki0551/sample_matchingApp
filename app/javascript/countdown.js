d789815abd59ac5776a4f395e55f2aea68964a80
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-03-19 14:22:33 +0900

    配列からメールアドレスを探して取得

--- app/javascript/countdown.js
+++ app/javascript/countdown.js
@@ -11,61 +11,59 @@ window.onload = function(){
 	targetTime = new Date();
 	targetTime.setMinutes(targetTime.getMinutes() + targetMinitues);
 	targetTime.setSeconds(targetTime.getSeconds() + targetSeconds);
-		
-	const endTime = targetTime.getTime();
+	let endTime = targetTime.getTime();
 
-	// localStorage.clear();
+	console.log(endTime)
 
-	const user_data_array = [];
-	let user_data = {
-		email: email,
-		endTime: endTime
-	}
+	 const user_data_array = [];
+	 let user_data = {
+		 email: email,
+		 endTime: endTime
+	 }
+	 user_data_array.push(user_data);
 
-	user_data_array.push(user_data)
-	// console.log(user_data_array[0])
+	 const serializedArray = JSON.stringify(user_data_array);
+	 localStorage.setItem('myArray', serializedArray);
 
-  // if (user_data_array[0]) {
-		function updateCountDown(){
-			const now = new Date().getTime();
-			// console.log(user_data_array[0]);
+	const serializedArray2 = localStorage.getItem('myArray');
+	const array = JSON.parse(serializedArray2);
+	console.log(array[0]);
 
-			function sort_user(user_data) {
-				return user_data.email === email;
-			}
-			console.log(user_data_array.find(sort_user));
 
-			const distance = user_data_array.email - now;
-	
-			const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-			const seconds = Math.floor(distance % (1000 * 60) / 1000);
-	
-			countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
-		
-			if(distance < 0){
-				clearInterval(interval);
-				message.textContent = "再度、新規登録できます。";
-			}
-		}
-		const interval = setInterval(updateCountDown, 1000);
-		updateCountDown();
-	// } else {
-	// 	function updateCountDown(){
-	// 		const now = new Date().getTime();
-	// 		const distance = endTime - now;
-	
-	// 		const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-	// 		const seconds = Math.floor(distance % (1000 * 60) / 1000);
-	
-	// 		countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
 
-	// 		if(distance < 0){
-	// 			clearInterval(interval);
-	// 			message.textContent = "再度、新規登録できます。";
-	// 		}
-	// 	}
-	
-	// 	const interval = setInterval(updateCountDown, 1000);
-	// 	updateCountDown();
+	// function sort_user(user_data) {
+	// 	return user_data.email === email;
 	// }
+
+
+//  if (user_data_array.find(sort_user)){
+// 		var target_user_data = user_data_array.find(sort_user);
+// 		endTime = target_user_data.endTime
+//  } else {
+// 	 const user_data_array = [];
+// 	 let user_data = {
+// 		 email: email,
+// 		 endTime: endTime
+// 	 }
+// 	 user_data_array.push(user_data)
+//  }
+
+
+
+	// function updateCountDown(){
+	// 	const now = new Date().getTime();
+	// 	const distance = target_user_data.endTime - now;
+
+	// 	const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+	// 	const seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+	// 	countDown.textContent = `${String(minutes)}分 ${String(seconds)}秒`;
+	
+	// 	if(distance < 0){
+	// 		clearInterval(interval);
+	// 		message.textContent = "再度、新規登録できます。";
+	// 	}
+	// }
+	// const interval = setInterval(updateCountDown, 1000);
+	// updateCountDown();
 }
