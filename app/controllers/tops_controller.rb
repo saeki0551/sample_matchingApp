9ee1ed774823b32d480d0000ab3f8d3c81b06735
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-03-22 07:13:04 +0900

    いらないコードの削除

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,10 +1,5 @@
 class TopsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :cancel_membership]
-
+  skip_before_action :authenticate_user!, only: %i[index]
   def index
-  end
-
-   def cancel_membership
-    @account_stop_time = ACCOUNT_STOP_TIME
   end
 end
