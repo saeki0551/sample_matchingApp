74280db2fbe309276005fd7f874a2619ef9eec09
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-03-31 06:26:39 +0900

    いいね機能非同期処理（他人のいいねが削除できてしまう）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,13 @@
 class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: params[:user_id])
+    @user = @like.user_id
     @like.save
   end
 
   def destroy
     @like = Like.find_by(user_id: params[:user_id], id: params[:id])
+    @user = @like.user_id
     @like.destroy
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_02_094011) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "liked_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_likes_on_user_id"
