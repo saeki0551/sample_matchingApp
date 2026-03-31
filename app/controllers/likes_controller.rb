e3916872fb1f5105d68755e0d618240d46891f0a
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-01 06:10:03 +0900

    @userの廃止 @like.user_idで代用

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,6 @@
 class LikesController < ApplicationController
   def create
     @like = Like.new(current_user_id: current_user.id, user_id: params[:user_id])
-    @user = @like.user_id
     unless @like.save
       redirect_to users_path, alert: "いいねができませんでした。"
     end
@@ -9,7 +8,6 @@ class LikesController < ApplicationController
 
   def destroy
     @like = Like.find_by(user_id: params[:user_id], id: params[:id])
-    @user = @like.user_id
     unless @like.destroy
       redirect_to users_path, alert: "いいねが削除できませんでした。"
     end
