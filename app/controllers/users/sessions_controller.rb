0a636d53d1f83f57ba1075760cc2985065d6f966
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-03 06:05:09 +0900

    パスワードなしでログインできないように修正。退会中ならログインできないように

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,20 +4,29 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
 
   # POST /resource/sign_in
   def create
-    user = User.order(updated_at: :desc).limit(1).find_by(email: params[:user][:email])
-    return redirect_to new_user_session_path, alert: "ログインできません。" if user.deleted_at
-    if sign_in(user)
-      redirect_to users_path, notice: "ログインしました"
-    else
-      redirect_to new_user_session_path, alert: "予想外のエラー、ログインに失敗しました。" 
+    self.resource = warden.authenticate!(auth_options)
+    binding.pry
+    if Time.zone.now - self.resource.deleted_at < ACCOUNT_STOP_TIME
+      # アカウント停止時間ないなのにログインできてしまう
+      sign_out(self.resource)
+      return redirect_to new_user_session_path, flash: { alert: "（退会中のため？）ログインできませんでした。" }
     end
+    # user = User.order(updated_at: :desc).limit(1).find_by(email: params[:user][:email])
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
+
 
   # DELETE /resource/sign_out
   # def destroy
