95c26b81c64355970991c97ce4c3baa3c1412c02
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-04 07:30:35 +0900

    ユーザーインスタンス取得の条件文の変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,23 +4,18 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
-  end
+  # def new
+  #   super
+  # end
+
 
   # POST /resource/sign_in
   def create
-    self.resource = warden.authenticate!(auth_options)
-    binding.pry
-    if self.resource.deleted_at
-      # アカウント停止時間ないなのにログインできてしまう
-      sign_out(self.resource)
+    resource = User.order(updated_at: :desc).find_by(email: params[:user][:email])
+    if resource.deleted_at
+      sign_out(resource)
       return redirect_to new_user_session_path, flash: { alert: "（退会中のため？）ログインできませんでした。" }
     end
-    # user = User.order(updated_at: :desc).limit(1).find_by(email: params[:user][:email])
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
     yield resource if block_given?
