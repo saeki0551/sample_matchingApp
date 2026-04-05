544b4425510c37614b02dd1f5d9b7aac684bc7b7
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-06 05:57:29 +0900

    パスワードがなくてもログインできたことを修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,18 +1,7 @@
-# frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-
-  # POST /resource/sign_in
   def create
-    パスワードなしでもログインできてしまう
-    resource = User.order(updated_at: :desc).find_by(email: params[:user][:email])
+    resource = warden.authenticate!(auth_options)
     if resource.deleted_at
       sign_out(resource)
       return redirect_to new_user_session_path, flash: { alert: "（退会中のため？）ログインできませんでした。" }
@@ -22,17 +11,4 @@ class Users::SessionsController < Devise::SessionsController
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
   end
-
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
