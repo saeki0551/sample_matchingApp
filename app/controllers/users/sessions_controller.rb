a1dd3974f1c3548c9b254871124d1ab0884eb2b3
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-06 16:33:32 +0900

    一度退会して新規登録してもログインできるように

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,15 @@
 
 class Users::SessionsController < Devise::SessionsController
   def create
-    resource = warden.authenticate!(auth_options)
+    user = User.order(id: :desc).find_by(email: params[:user][:email])
+    if user&.valid_password?(params[:user][:password])
+      resource = user
+    else
+      flash.now[:alert] = 'メールアドレスまたはパスワードが正しくありません。'
+      render :new
+    end
     # 一度退会したらログインできなくなってしまう
-    if resource.deleted_at
-      sign_out(resource)
+    if resource.deleted_at.present?
       return redirect_to new_user_session_path, flash: { alert: "（退会中のため？）ログインできませんでした。" }
     end
     set_flash_message!(:notice, :signed_in)
