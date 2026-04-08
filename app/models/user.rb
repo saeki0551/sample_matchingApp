091a81d8b05c2f6184cf18b6c1f71d59704a3056
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-09 05:54:53 +0900

    キー＋バリュー

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,10 @@ class User < ApplicationRecord
     User.exists?(email: email.values, deleted_at: nil) 
   end
   
-  def check_password(password)
-    return "パスワード は英数字である必要があります。" unless /\A[a-zA-Z\d]+\z/.match(password.values[0])
-    return "パスワード は6文字以上12文字以内である必要があります。" unless password.values[0].length >= 6 && password.values[0].length <= 12
-    return "パスワード と パスワード確認 が一致していません。" unless password.values[0] == password.values[1]
+  def check_password(**password)
+    return "パスワード は英数字である必要があります。" unless /\A[a-zA-Z\d]+\z/.match(password[:password])
+    return "パスワード は6文字以上12文字以内である必要があります。" unless password[:password].length >= 6 && password[:password].length <= 12
+    return "パスワード と パスワード確認 が一致していません。" unless password[:password] == password[:password_confirmation]
   end
   
   def self.in_time_cancel_membership?(deleted_at, account_stop_time)
