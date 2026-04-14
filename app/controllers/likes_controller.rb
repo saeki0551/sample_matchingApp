044aad4b2416722cc0efa984460ff17ee2bd3461
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-14 19:27:59 +0900

    文字列はシングルクォーテーションに

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,14 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: current_user.id, liked_user_id: params[:user_id])
     unless @like.save
-      redirect_to users_path, alert: "いいねができませんでした。"
+      redirect_to users_path, alert: 'いいねができませんでした。'
     end
   end
 
   def destroy
     @like = Like.find(params[:id])
     unless @like.destroy
-      redirect_to users_path, alert: "いいねが削除できませんでした。"
+      redirect_to users_path, alert: 'いいねが削除できませんでした。'
     end
   end
 
