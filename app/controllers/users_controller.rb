74cd9384d71a4deeb6a882f938e01e5849782412
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-14 19:59:12 +0900

    文字列はシングルクォーテーションに

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,20 +11,20 @@ class UsersController < ApplicationController
 
   def destroy
     user = User.find(params[:id])
-    return redirect_to users_path, alert: "ユーザーidが一致しないため、退会ができません。"  unless user.id == current_user.id
+    return redirect_to users_path, alert: 'ユーザーidが一致しないため、退会ができません。'  unless user.id == current_user.id
 
     if user.update(deleted_at: DateTime.now)
       session[:ensure_user_id] = user.id
       sign_out(user)
       redirect_to cancel_membership_user_path(user)
     else
-      redirect_to users_path, alert: "予想外のエラー、退会に失敗しました。"
+      redirect_to users_path, alert: '退会に失敗しました。'
     end
   end
 
   def cancel_membership
     @account_stop_time = ACCOUNT_STOP_TIME
     @user = User.find(params[:id])
-    return redirect_to new_user_session_path, alert: "ユーザーidが一致していません。"  unless @user.id == session[:ensure_user_id]
+    return redirect_to new_user_session_path, alert: 'ユーザーidが一致していません。'  unless @user.id == session[:ensure_user_id]
   end
 end
