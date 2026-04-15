fe225e01aa7311f4acdfac127079c2da9c4f50f4
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-04-16 06:26:40 +0900

    current_userがあるときは current_user.で

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new(user_id: current_user.id, liked_user_id: params[:user_id])
+    @like = current_user.likes.new(liked_user_id: like_params[:user_id])
     unless @like.save
       redirect_to users_path, alert: 'いいねができませんでした。'
     end
   end
 
   def destroy
-    @like = Like.find(params[:id])
+    @like = Like.find(like_params[:id])
     unless @like.destroy
       redirect_to users_path, alert: 'いいねが削除できませんでした。'
     end
