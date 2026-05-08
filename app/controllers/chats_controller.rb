8f056c253d81c1e3e69be0ebc25f9e25753f6df2
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-05-09 06:17:45 +0900

    chatのnewアクションとviewの作成

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,16 @@
 class ChatsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new]
+  skip_before_action :authenticate_user!, only: [:new, :create]
+  
   def new
+    @chat = Chat.new
+  end
+  
+  def create
+    @chat = Chat.new
+  end
+
+  def user_params
     binding.pry
+    params.require(:user).permit(:id)
   end
 end
