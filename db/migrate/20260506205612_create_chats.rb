f1cb80ba11c5b07510151f2779ec9d6b1009bc4c
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-05-11 06:25:38 +0900

    chatしているユーザーidのカラム削除

--- db/migrate/20260506205612_create_chats.rb
+++ db/migrate/20260506205612_create_chats.rb
@@ -2,7 +2,6 @@ class CreateChats < ActiveRecord::Migration[8.0]
   def change
     create_table :chats do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :chatting_user_id, null: false
       t.string :text
       
       t.timestamps
