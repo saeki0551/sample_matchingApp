0bd69bd8e18096ff25f7380860315f8e9166edc5
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-06-19 06:32:16 +0900

    自分をいいねしているユーザーを取得。エラー処理も実装。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,7 +4,15 @@ class LikesController < ApplicationController
     unless @like.save
       redirect_to users_path, alert: 'いいねができませんでした。'
     end
-
+    begin
+      matching_user = current_user.liked_users.find_by(user_id: like_params[:user_id])
+    rescue => e
+      logger.error(e.message)
+      return redirect_to users_path, flash: {alert: "いいねするユーザーが見つかりません。"}
+    end
+    if matching_user.liked_user?(user_id: like_params[:user_ids][0])
+      redirect_to user_path(matching_user), notice: 'マッチングしました。'
+    end
   end
 
   def destroy
