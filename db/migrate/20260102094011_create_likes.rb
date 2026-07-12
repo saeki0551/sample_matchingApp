9ecb3fde8a11070e1737407a8eb4cdaaaf44e788
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-07-13 07:46:12 +0900

    外部キーの実装（実装できていなかった）

--- db/migrate/20260102094011_create_likes.rb
+++ db/migrate/20260102094011_create_likes.rb
@@ -2,8 +2,9 @@ class CreateLikes < ActiveRecord::Migration[8.0]
   def change
     create_table :likes do |t|
       t.references :user, foreign_key: true
-      t.references :liked_user, null: false
+      t.references :liked_user
       t.timestamps
     end
+    add_foreign_key :likes, :users, column: :liked_user_id
   end
 end
