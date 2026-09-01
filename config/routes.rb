266043c2f2e89615235650ed23194f24c2e24b3b
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-09-01 14:34:46 +0900

    idを渡さなくても詳細画面を表示・非表示（１人ずつしか表示しないため）

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,14 @@ Rails.application.routes.draw do
     post 'users', to: 'users/registrations#create', as: :create_user_registration
   end
   root "tops#index"
+  
+  delete 'users/remove', to: 'users#remove', as: :remove_user
 
   resources :users do
     resources :likes, only: [:create, :destroy]
   end
   
   delete "users/:id/destroy", to: "users#destroy", as: :destroy_user
-  delete 'users/:id/remove', to: 'users#remove', as: :remove_user
   get "users/:id/cancel_membership", to: "users#cancel_membership", as: :cancel_membership_user
   
   get 'user_informations/new', to: 'user_informations#new', as: :new_user_information
