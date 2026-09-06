f330f6a93b952722ddf0827100d75c045d4adc01
SaekiNaohiko <nsaekitadayayu@icloud.com>
2026-09-07 07:37:34 +0900

    システムエラーはrescueしない

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,8 @@ class LikesController < ApplicationController
   def create
     begin
       like = current_user.likes.create!(liked_user_id: params[:user_id])
-    rescue => e
-      logger.error(e.message)
-      if e.class == ActiveRecord::RecordInvalid
-        return redirect_to users_path, flash: {alert: 'いいねする相手が存在しません。'}
-      else
-        return redirect_to system_error_path
-      end
+    rescue ActiveRecord::RecordInvalid
+      return redirect_to users_path, flash: {alert: 'いいねする相手が存在しません。'}
     end
     if current_user.liked_users.exists?(user_id: like.liked_user_id)
       redirect_to user_path(like.liked_user_id), notice: 'マッチングしました。'
